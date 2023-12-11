bd17e298dad1fc86ff64aed48a71b59c6341fb5c
Krishna-1899 <rajeshbhanushali189@gmail.com>
2023-12-11 10:41:16 +0530

    final commit

--- controllers/Users/DeleteUserById.js
+++ controllers/Users/DeleteUserById.js
@@ -4,33 +4,45 @@ const userModel = require("../../services/UserModels");
 const Response=require("../../utils/Response");
 exports.deleteUserById=async(req,res)=>{
     const {id}=req.body;
-    console.log(id);
     if(!id){
-        return Response.invalidInput(res,"user is not found");
+        return Response.invalidInput(res,"User is not found");
     }
+    console.log(id);
     try{
         userModel.getUserById(id,(user)=>{
             console.log(user);
             if(user){
                 const path=user.profilePath;
-                {
-                    path && userModel.deleteUser(id,()=>{
-                        fs.remove(path)
-                        .then(() => {
-                            console.log('success!');
-                        })
-                        .catch(err => {
-                            return Response.sendFailed(res,err.message);
-                        })
-                    });
-                }
-                Response.sendSuccess(res,"User deleted successfully");
+                userModel.deleteUser(id,()=>{
+                    if(path) {
+                        fs.exists(path)
+                            .then(() => fs.remove(path))
+                            .catch((err) => {
+                                console.error(`Error while checking or removing file: ${err.message}`);
+                                throw err;
+                            });
+                    }                    
+                    Response.sendSuccess(res,"User deleted successfully");
+                });
+                // {
+                //     path && userModel.deleteUser(id,()=>{
+                //         fs.remove(path)
+                //         .then(() => {
+                //             console.log('success!');
+                //         })
+                //         .catch(err => {
+                //             return Response.sendFailed(res,err.message);
+                //         })
+                //     });
+                // }
+                // Response.sendSuccess(res,"User deleted successfully");
             }
             else{
                 return Response.invalidInput(res,"user is not found");
             }
         })
     }catch(err){
+        console.log("errror from catch block",err.message);
         return Response.sendFailed(res,err.message);
     }
 }
